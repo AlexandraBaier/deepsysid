96122c1deeda2edfae362e85ee3d7aa5fc4ca7bf
Alex Baier <alx.baier@gmail.com>
2022-05-08 10:30:09 +0200

    Adjusted sysid.execution to match corrected type hints of DynamicIdentificationModel. Adjusted imports and usage of type hints according to changed practices. No more T.List etc. instead type hint directly List

--- multistep_sysid/execution.py
+++ multistep_sysid/execution.py
@@ -1,16 +1,13 @@
-from collections import namedtuple
 import os
-import typing as T
+from typing import List, Tuple, Type
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics import mean_absolute_error
 
-from .models import base
-from . import utils
+from .models.base import DynamicIdentificationModel
 
 
-def load_control_and_state(file_path: str, control_names: T.List[str], state_names: T.List[str]) -> T.Tuple[
+def load_control_and_state(file_path: str, control_names: List[str], state_names: List[str]) -> Tuple[
     np.array, np.array]:
     sim = pd.read_csv(file_path)
     control_df = sim[control_names]
@@ -19,8 +16,9 @@ def load_control_and_state(file_path: str, control_names: T.List[str], state_nam
     return control_df.values, state_df.values
 
 
-def load_simulation_data(directory: str, control_names: T.List[str], state_names: T.List[str]) -> T.Tuple[
-    T.List[np.array], T.List[np.array]]:
+def load_simulation_data(
+        directory: str, control_names: List[str], state_names: List[str]
+) -> Tuple[List[np.array], List[np.array]]:
     file_names = load_file_names(directory)
 
     controls = []
@@ -32,7 +30,7 @@ def load_simulation_data(directory: str, control_names: T.List[str], state_names
     return controls, states
 
 
-def load_file_names(directory: str) -> T.List[str]:
+def load_file_names(directory: str) -> List[str]:
     file_names = []
     for fn in os.listdir(directory):
         if fn.endswith('.csv'):
@@ -40,23 +38,17 @@ def load_file_names(directory: str) -> T.List[str]:
     return sorted(file_names)
 
 
-def load_model(model: base.DynamicIdentificationModel, directory: str, model_name: str):
+def load_model(model: DynamicIdentificationModel, directory: str, model_name: str):
     extension = model.get_file_extension()
-    if type(extension) == str:
-        model.load(os.path.join(directory, f'{model_name}.{extension}'))
-    else:
-        model.load([os.path.join(directory, f'{model_name}.{ext}') for ext in extension])
+    model.load(tuple(os.path.join(directory, f'{model_name}.{ext}') for ext in extension))
 
 
-def save_model(model: base.DynamicIdentificationModel, directory: str, model_name: str):
+def save_model(model: DynamicIdentificationModel, directory: str, model_name: str):
     extension = model.get_file_extension()
-    if type(extension) == str:
-        model.save(os.path.join(directory, f'{model_name}.{extension}'))
-    else:
-        model.save([os.path.join(directory, f'{model_name}.{ext}') for ext in extension])
+    model.save(tuple(os.path.join(directory, f'{model_name}.{ext}') for ext in extension))
 
 
-def retrieve_model_class(model_class_string: str) -> T.Type[base.DynamicIdentificationModel]:
+def retrieve_model_class(model_class_string: str) -> Type[DynamicIdentificationModel]:
     # https://stackoverflow.com/a/452981
     parts = model_class_string.split('.')
     module_string = '.'.join(parts[:-1])
@@ -65,40 +57,3 @@ def retrieve_model_class(model_class_string: str) -> T.Type[base.DynamicIdentifi
     for component in parts[1:]:
         module = getattr(module, component)
     return module
-
-
-class MAEEpochValidator(object):
-    ValidationSet = namedtuple('ValidationSet', ('initial_control', 'initial_state', 'control', 'state'))
-
-    def __init__(self, controls: T.List[np.array], states: T.List[np.array], window_size: int, horizon_size: int,
-                 trigger_after_epoch: int = 5):
-        self.after_epoch = trigger_after_epoch  # type: int
-        self.state_dim = states[0].shape[1]
-
-        self.validation_set = []  # type: T.List[MAEEpochValidator.ValidationSet]
-
-        total_length = window_size + horizon_size
-        for control, state in zip(controls, states):
-            for i in range(total_length, control.shape[0], total_length):
-                initial_control = control[i - total_length:i - total_length + window_size]
-                initial_state = state[i - total_length:i - total_length + window_size]
-                true_control = control[i - total_length + window_size:i]
-                true_state = state[i - total_length + window_size:i]
-
-                self.validation_set.append(
-                    MAEEpochValidator.ValidationSet(initial_control, initial_state, true_control, true_state))
-
-    def __call__(self, model: base.DynamicIdentificationModel, epoch: int) -> T.Optional[T.List[float]]:
-        if epoch == 0 or ((epoch+1) % self.after_epoch) != 0:
-            return None
-
-        try:
-            error = np.mean(utils.score_on_sequence(
-                [vs.state for vs in self.validation_set],
-                [model.simulate(vs.initial_control, vs.initial_state, vs.control) for vs in self.validation_set],
-                lambda t, p: mean_absolute_error(t, p, multioutput='raw_values')
-            ), axis=0)
-        except ValueError:
-            error = []
-
-        return list(error)
