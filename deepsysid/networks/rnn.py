d77a76cadbfcb04e0a5ff80170ec8959be09e4ce
Daniel Frank <daniel.frank@ipvs.uni-stuttgart.de>
2022-07-13 00:37:32 +0200

    Add constrained RNN network

--- deepsysid/networks/rnn.py
+++ deepsysid/networks/rnn.py
@@ -3,6 +3,8 @@ from typing import List, Optional, Tuple, Union
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import cvxpy as cp
+import numpy as np
 
 
 class BasicLSTM(nn.Module):
@@ -114,3 +116,175 @@ class LinearOutputLSTM(nn.Module):
             return x, (h0, c0)
         else:
             return x
+
+class LTIRnn(nn.Module):
+
+    def __init__(
+        self, 
+        nx: int,
+        nu: int,
+        ny: int,
+        nw: int,
+        gamma: int,
+        beta: float
+    ) -> None:
+        super(LTIRnn, self).__init__()
+
+        self.nx = nx # number of states
+        self.nu = nu # number of performance (external) input
+        self.nw = nw # number of disturbance input
+        self.ny = ny # number of performance output
+        self.nz = nw # number of disturbance output, always equal to size of w
+
+        self.ga = gamma
+        self.beta = beta
+        self.nl = torch.tanh
+
+        self.Y = torch.nn.Parameter(torch.zeros((self.nx, self.nx)))
+        self.A_tilde = torch.nn.Linear(self.nx, self.nx, bias=False)
+        self.B1_tilde = torch.nn.Linear(self.nu, self.nx, bias=False)
+        self.B2_tilde = torch.nn.Linear(self.nw, self.nx, bias=False)
+        self.C1 = torch.nn.Linear(self.nx, self.ny, bias=False)
+        self.D11 = torch.nn.Linear(self.nu, self.ny, bias=False)
+        self.D12 = torch.nn.Linear(self.nw, self.ny, bias=False)
+        self.C2_tilde = torch.nn.Linear(self.nx, self.nz, bias=False)
+        self.D21_tilde = torch.nn.Linear(self.nu, self.nz, bias=False)
+        self.lambdas = torch.nn.Parameter(torch.zeros((self.nw,1)))
+
+
+    def initialize_lmi(self) -> None:
+        # storage function
+        Y = cp.Variable((self.nx, self.nx), 'Y')
+        # hidden state
+        A_tilde = cp.Variable((self.nx, self.nx), 'A_tilde')
+        B1_tilde = cp.Variable((self.nx, self.nu), 'B1_tilde')
+        B2_tilde = cp.Variable((self.nx, self.nw), 'B2_tilde')
+        # output
+        C1 = cp.Variable((self.ny, self.nx), 'C1')
+        D11 = cp.Variable((self.ny, self.nu), 'D11')
+        D12 = cp.Variable((self.ny, self.nw), 'D12')
+        # disturbance
+        C2 = np.random.normal(0,1/np.sqrt(self.nw), size=(self.nz, self.nx))
+        D21 = np.random.normal(0,1/np.sqrt(self.nw), size=(self.nz, self.nu))
+        # multipliers
+        lambdas = cp.Variable((self.nw,1), 'tau', nonneg=True)
+        T = cp.diag(lambdas)
+
+        C2_tilde = T @ C2
+        D21_tilde = T @ D21
+
+        # lmi that ensures l2 gain
+        M = cp.bmat([ [ -Y,             np.zeros((self.nx,self.nu)),       self.beta*C2_tilde.T,        A_tilde.T,         C1.T],
+            [np.zeros((self.nu,self.nx)),    -self.ga**2*np.eye(self.nu),        self.beta*D21_tilde.T,       B1_tilde.T,        D11.T],
+            [self.beta*C2_tilde,          self.beta * D21_tilde,              -2*T,                   B2_tilde.T,        D12.T],
+            [A_tilde,                B1_tilde,                      B2_tilde,               -Y,                     np.zeros((self.nx, self.ny))],
+            [C1,                    D11,                            D12,                    np.zeros((self.ny, self.nx)),     -np.eye(self.ny)]])
+
+        # setup optimization problem, objective might change, any feasible solution works as initialization for the parameters
+        nM = M.shape[0]
+        tol = 1e-4
+        # rand_matrix = np.random.normal(0,1/np.sqrt(self.nx), (self.nx,self.nw))
+        # objective = cp.Minimize(cp.norm(Y @ rand_matrix - B2_tilde))
+        objective = cp.Minimize(None)
+        problem = cp.Problem(objective, [M << -tol*np.eye(nM)])
+        
+        print(f'Initialize Parameter by values that satisfy LMI constraints, solve SDP ...')
+        problem.solve(solver=cp.MOSEK)
+        # check if t is negative
+        max_eig_lmi = np.max(np.real(np.linalg.eig(M.value)[0]))
+        if max_eig_lmi < 0 and problem.status == 'optimal':
+            # print(f'Found negative semidefinite lmi, problem status: \t {problem.status} \t max eig(LMI)={max_eig_lmi:3f}, \t t={t[0].value:3f}')
+            print(f'Found negative semidefinite LMI, problem status: \t {problem.status} \t max eig(LMI)={max_eig_lmi:3f}')
+        else:
+            raise Exception("Neural network could not be initialized since no solution to the SDP problem was found.")
+
+        print(f'Write back Parameters values ...')
+        dtype = torch.get_default_dtype()
+        
+        self.Y = torch.nn.Parameter(torch.tensor(Y.value, dtype=dtype))
+        self.A_tilde.weight = torch.nn.Parameter(torch.tensor(A_tilde.value, dtype=dtype))
+        
+        self.B1_tilde.weight = torch.nn.Parameter(torch.tensor(B1_tilde.value, dtype=dtype))
+        self.B2_tilde.weight = torch.nn.Parameter(torch.tensor(B2_tilde.value, dtype=dtype))
+        self.C1.weight = torch.nn.Parameter(torch.tensor(C1.value, dtype=dtype))
+        self.D11.weight = torch.nn.Parameter(torch.tensor(D11.value, dtype=dtype))
+        self.D12.weight = torch.nn.Parameter(torch.tensor(D12.value, dtype=dtype))
+        self.C2_tilde.weight = torch.nn.Parameter(torch.tensor(C2_tilde.value, dtype=dtype)) 
+        self.D21_tilde.weight = torch.nn.Parameter(torch.tensor(D21_tilde.value, dtype=dtype))
+        self.lambdas = torch.nn.Parameter(torch.tensor(lambdas.value, dtype=dtype))
+
+    def forward(self, u_tilde, hx):
+            n_batch, n_sample, _ = u_tilde.shape
+
+            Y_inv = self.Y.inverse()
+            T_inv = torch.diag(1/torch.squeeze(self.lambdas))
+            # initialize output
+            y = torch.zeros((n_batch, n_sample,self.ny))
+
+            # print(f'Shape of input u_tilde: {u_tilde.shape} \t shape of hx: {hx[0].shape}')
+            x = hx[0][1]
+            for k in range(n_sample):
+                z = (self.C2_tilde(x) + self.D21_tilde(u_tilde[:,k,:])) @ T_inv
+                w = self.nl(z)
+                y[:, k, :] = self.C1(x) + self.D11(u_tilde[:,k,:]) + self.D12(w)
+                x = (self.A_tilde(x) + self.B1_tilde(u_tilde[:,k,:]) + self.B2_tilde(w)) @ Y_inv
+        
+            return y      
+
+    def get_constraints(self):
+        # state sizes
+        nx = self.nx
+        nu = self.nu
+        ny = self.ny
+
+        beta = self.beta
+        # storage function
+        Y = self.Y
+
+        # state
+        A_tilde = self.A_tilde.weight
+        B1_tilde = self.B1_tilde.weight
+        B2_tilde = self.B2_tilde.weight
+        # output
+        C1 = self.C1.weight
+        D11 = self.D11.weight
+        D12 = self.D12.weight 
+        # disturbance
+        D21_tilde = self.D21_tilde.weight
+        C2_tilde = self.C2_tilde.weight
+        
+        T = torch.diag(torch.squeeze(self.lambdas))
+        ga = self.ga
+
+        # M << 0
+        M = torch.cat([ torch.cat([-Y,                                      torch.zeros((nx,nu)),   beta * C2_tilde.T,      A_tilde.T,                       C1.T], axis=1),
+                torch.cat([torch.zeros((nu,nx)),        -ga**2*torch.eye(nu),  beta * D21_tilde.T,     B1_tilde.T,                      D11.T], axis=1),
+                torch.cat([beta * C2_tilde,                                 beta * D21_tilde,                           -2*T,                   B2_tilde.T,                      D12.T], axis=1),
+                torch.cat([A_tilde,                                         B1_tilde,                                   B2_tilde,               -Y,                             torch.zeros((nx, ny))], axis=1),
+                torch.cat([C1,                                              D11,                                        D12,                    torch.zeros((ny, nx)),     -torch.eye(ny)], axis=1)])
+
+        return 0.5 * (M + M.T)
+    
+    def get_barrier(self, t):
+        M = self.get_constraints()
+        # print(f'barrier {-t*(-M).logdet()}, \t type of barrier {(-t*(-M).logdet()).requires_grad}')
+        barrier = -t*(-M).logdet()
+        # print(f'barrier {-t*(-M).logdet()}')
+
+        try:
+            torch.linalg.cholesky(-M)
+        except:
+            barrier += torch.tensor(float('inf'))
+
+        return barrier
+
+    def check_constr(self):
+        with torch.no_grad():
+            M = self.get_constraints()
+            try:
+                torch.linalg.cholesky(-M)
+                b_satisfied = True
+            except:
+                b_satisfied = False
+
+        return b_satisfied
