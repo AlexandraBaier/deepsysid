ab38a82aef62fa572807d426a76e2d527e1cc177
Alexandra Baier <alx.baier@gmail.com>
2022-08-15 11:42:10 +0200

    Debugged LSTMCombinedInitModel and added smoke test.

--- src/deepsysid/models/recurrent.py
+++ src/deepsysid/models/recurrent.py
@@ -524,7 +524,8 @@ class LSTMInitModel(base.DynamicIdentificationModel):
         )
         return init_count + predictor_count
 
-class LSTMInitModelCombinedConfig(DynamicIdentificationModelConfig):
+
+class LSTMCombinedInitModelConfig(DynamicIdentificationModelConfig):
     recurrent_dim: int
     num_recurrent_layers: int
     dropout: float
@@ -533,10 +534,12 @@ class LSTMInitModelCombinedConfig(DynamicIdentificationModelConfig):
     batch_size: int
     epochs: int
     loss: Literal['mse', 'msge']
-class LSTMInitModelCombined(base.DynamicIdentificationModel):
-    CONFIG = LSTMInitModelCombinedConfig
 
-    def __init__(self, config: LSTMInitModelCombinedConfig):
+
+class LSTMCombinedInitModel(base.DynamicIdentificationModel):
+    CONFIG = LSTMCombinedInitModelConfig
+
+    def __init__(self, config: LSTMCombinedInitModelConfig):
         super().__init__(config)
 
         self.device_name = config.device_name
@@ -568,9 +571,7 @@ class LSTMInitModelCombined(base.DynamicIdentificationModel):
             output_dim=self.state_dim,
             dropout=self.dropout,
         ).to(self.device)
-        self.optimizer = optim.Adam(
-            self.predictor.parameters(), lr=self.learning_rate
-        )
+        self.optimizer = optim.Adam(self.predictor.parameters(), lr=self.learning_rate)
 
         self.state_mean: Optional[np.ndarray] = None
         self.state_std: Optional[np.ndarray] = None
@@ -580,15 +581,15 @@ class LSTMInitModelCombined(base.DynamicIdentificationModel):
     def train(self, control_seqs, state_seqs):
         self.predictor.train()
 
-        self.control_mean, self.control_stddev = utils.mean_stddev(control_seqs)
-        self.state_mean, self.state_stddev = utils.mean_stddev(state_seqs)
+        self.control_mean, self.control_std = utils.mean_stddev(control_seqs)
+        self.state_mean, self.state_std = utils.mean_stddev(state_seqs)
 
         control_seqs = [
-            utils.normalize(control, self.control_mean, self.control_stddev)
+            utils.normalize(control, self.control_mean, self.control_std)
             for control in control_seqs
         ]
         state_seqs = [
-            utils.normalize(state, self.state_mean, self.state_stddev)
+            utils.normalize(state, self.state_mean, self.state_std)
             for state in state_seqs
         ]
 
@@ -609,29 +610,41 @@ class LSTMInitModelCombined(base.DynamicIdentificationModel):
                 predictor_dataset, self.batch_size, shuffle=True, drop_last=True
             )
             total_loss = 0.0
-            for batch_idx, (batch_init, batch_pred) in enumerate(zip(data_loader_init, data_loader_pred)):
+            total_init_loss = 0.0
+            total_pred_loss = 0.0
+            for batch_idx, (batch_init, batch_pred) in enumerate(
+                zip(data_loader_init, data_loader_pred)
+            ):
 
-                y, h0 = self.predictor.forward(batch_pred['x'].float().to(self.device), x0=batch_init['x'].float().to(self.device), return_init=True)
+                y, h0 = self.predictor.forward(
+                    batch_pred['x'].float().to(self.device),
+                    x0=batch_init['x'].float().to(self.device),
+                    return_init=True,
+                )
 
-                batch_loss_init = self.loss.forward(h0, batch_init['y'].float().to(self.device))
-                batch_loss_pred = self.loss.forward(y, batch_pred['y'].float().to(self.device))
+                batch_loss_init = self.loss.forward(
+                    h0, batch_init['y'].float().to(self.device)
+                )
+                batch_loss_pred = self.loss.forward(
+                    y, batch_pred['y'].float().to(self.device)
+                )
                 batch_loss = batch_loss_init + batch_loss_pred
+                total_init_loss += batch_loss_init.item()
+                total_pred_loss += batch_loss_pred.item()
                 total_loss += batch_loss.item()
                 batch_loss.backward()
                 self.optimizer.step()
 
             logger.info(
                 f'Epoch {i + 1}/{self.epochs} \t'
-                f'Epoch Loss : {total_loss:1f}\t'
-                f'Init Loss: {batch_loss_init:1f}\t'
-                f'Prediction Loss: {batch_loss_pred:1f}'
+                f'Epoch Loss : {total_loss:.4f}\t'
+                f'Init Loss: {total_init_loss:.4f}\t'
+                f'Prediction Loss: {total_pred_loss:.4f}'
             )
         time_end = time.time()
-       
+
         time_total = time_end - time_start
-        logger.info(
-            f'Training time for predictor {time_total}s '
-        )
+        logger.info(f'Training time for predictor {time_total}s ')
 
     def simulate(
         self,
@@ -639,16 +652,13 @@ class LSTMInitModelCombined(base.DynamicIdentificationModel):
         initial_state: np.ndarray,
         control: np.ndarray,
     ) -> np.ndarray:
-        self.initializer.eval()
         self.predictor.eval()
 
         initial_control = utils.normalize(
-            initial_control, self.control_mean, self.control_stddev
+            initial_control, self.control_mean, self.control_std
         )
-        initial_state = utils.normalize(
-            initial_state, self.state_mean, self.state_stddev
-        )
-        control = utils.normalize(control, self.control_mean, self.control_stddev)
+        initial_state = utils.normalize(initial_state, self.state_mean, self.state_std)
+        control = utils.normalize(control, self.control_mean, self.control_std)
 
         with torch.no_grad():
             init_x = (
@@ -659,18 +669,18 @@ class LSTMInitModelCombined(base.DynamicIdentificationModel):
             )
             pred_x = torch.from_numpy(control).unsqueeze(0).float().to(self.device)
 
-            y = self.predictor.forward(pred_x, h0=init_x, return_state=False)
+            y = self.predictor.forward(pred_x, x0=init_x)
             y_np = y.cpu().detach().squeeze().numpy()  # type: ignore
 
-        y_np = utils.denormalize(y_np, self.state_mean, self.state_stddev)
+        y_np = utils.denormalize(y_np, self.state_mean, self.state_std)
         return y_np
 
     def save(self, file_path: Tuple[str, ...]):
         if (
             self.state_mean is None
-            or self.state_stddev is None
+            or self.state_std is None
             or self.control_mean is None
-            or self.control_stddev is None
+            or self.control_std is None
         ):
             raise ValueError('Model has not been trained and cannot be saved.')
 
@@ -679,9 +689,9 @@ class LSTMInitModelCombined(base.DynamicIdentificationModel):
             json.dump(
                 {
                     'state_mean': self.state_mean.tolist(),
-                    'state_stddev': self.state_stddev.tolist(),
+                    'state_stddev': self.state_std.tolist(),
                     'control_mean': self.control_mean.tolist(),
-                    'control_stddev': self.control_stddev.tolist(),
+                    'control_stddev': self.control_std.tolist(),
                 },
                 f,
             )
@@ -693,22 +703,18 @@ class LSTMInitModelCombined(base.DynamicIdentificationModel):
         with open(file_path[1], mode='r') as f:
             norm = json.load(f)
         self.state_mean = np.array(norm['state_mean'])
-        self.state_stddev = np.array(norm['state_stddev'])
+        self.state_std = np.array(norm['state_stddev'])
         self.control_mean = np.array(norm['control_mean'])
-        self.control_stddev = np.array(norm['control_stddev'])
+        self.control_std = np.array(norm['control_stddev'])
 
     def get_file_extension(self) -> Tuple[str, ...]:
         return 'predictor.pth', 'json'
 
     def get_parameter_count(self) -> int:
-        # technically parameter counts of both networks are equal
-        init_count = sum(
-            p.numel() for p in self.initializer.parameters() if p.requires_grad
-        )
         predictor_count = sum(
             p.numel() for p in self.predictor.parameters() if p.requires_grad
         )
-        return init_count + predictor_count
+        return predictor_count
 
 
 class _InitializerDataset(data.Dataset):
