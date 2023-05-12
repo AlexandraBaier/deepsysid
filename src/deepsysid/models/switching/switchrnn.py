Fixed bug in which explainer uses incorrect dimensions to construct ReLiNet explanations. (#44)

src/deepsysid/models/switching/switchrnn.py
@@ -39,7 +39,7 @@ class SwitchingLSTMBaseModel(base.DynamicIdentificationModel):
         self.device = torch.device(config.device_name)
 
         self.control_dim = len(config.control_names)
-        self.state_dim = len(config.state_names)
+        self.output_dim = len(config.state_names)
 
         self.recurrent_dim = config.recurrent_dim
         self.num_recurrent_layers = config.num_recurrent_layers
@@ -61,10 +61,10 @@ class SwitchingLSTMBaseModel(base.DynamicIdentificationModel):
         self.predictor = predictor.to(self.device)
 
         self.initializer = rnn.BasicLSTM(
-            input_dim=self.control_dim + self.state_dim,
+            input_dim=self.control_dim + self.output_dim,
             recurrent_dim=self.recurrent_dim,
             num_recurrent_layers=self.num_recurrent_layers,
-            output_dim=[self.state_dim],
+            output_dim=[self.output_dim],
             dropout=self.dropout,
         ).to(self.device)
 
@@ -77,8 +77,8 @@ class SwitchingLSTMBaseModel(base.DynamicIdentificationModel):
 
         self.control_mean: Optional[NDArray[np.float64]] = None
         self.control_std: Optional[NDArray[np.float64]] = None
-        self.state_mean: Optional[NDArray[np.float64]] = None
-        self.state_std: Optional[NDArray[np.float64]] = None
+        self.output_mean: Optional[NDArray[np.float64]] = None
+        self.output_std: Optional[NDArray[np.float64]] = None
 
     def train(
         self,
@@ -93,14 +93,14 @@ class SwitchingLSTMBaseModel(base.DynamicIdentificationModel):
         self.initializer.train()
 
         self.control_mean, self.control_std = utils.mean_stddev(control_seqs)
-        self.state_mean, self.state_std = utils.mean_stddev(state_seqs)
+        self.output_mean, self.output_std = utils.mean_stddev(state_seqs)
 
         control_seqs = [
             utils.normalize(control, self.control_mean, self.control_std)
             for control in control_seqs
         ]
         state_seqs = [
-            utils.normalize(state, self.state_mean, self.state_std)
+            utils.normalize(state, self.output_mean, self.output_std)
             for state in state_seqs
         ]
 
@@ -184,8 +184,8 @@ class SwitchingLSTMBaseModel(base.DynamicIdentificationModel):
         x0: Optional[NDArray[np.float64]] = None,
     ) -> Tuple[NDArray[np.float64], Dict[str, NDArray[np.float64]]]:
         if (
-            self.state_mean is None
-            or self.state_std is None
+            self.output_mean is None
+            or self.output_std is None
             or self.control_mean is None
             or self.control_std is None
         ):
@@ -200,7 +200,9 @@ class SwitchingLSTMBaseModel(base.DynamicIdentificationModel):
         initial_control = utils.normalize(
             initial_control, self.control_mean, self.control_std
         )
-        initial_state = utils.normalize(initial_state, self.state_mean, self.state_std)
+        initial_state = utils.normalize(
+            initial_state, self.output_mean, self.output_std
+        )
         control = utils.normalize(control, self.control_mean, self.control_std)
 
         with torch.no_grad():
@@ -226,7 +228,7 @@ class SwitchingLSTMBaseModel(base.DynamicIdentificationModel):
                 output.control_matrices.cpu().detach().squeeze().numpy()
             )
 
-        y_np = utils.denormalize(y_np, self.state_mean, self.state_std)
+        y_np = utils.denormalize(y_np, self.output_mean, self.output_std)
 
         return y_np.astype(np.float64), dict(
             system_matrices=np.array(system_matrices, dtype=np.float64),
@@ -235,8 +237,8 @@ class SwitchingLSTMBaseModel(base.DynamicIdentificationModel):
 
     def save(self, file_path: Tuple[str, ...]) -> None:
         if (
-            self.state_mean is None
-            or self.state_std is None
+            self.output_mean is None
+            or self.output_std is None
             or self.control_mean is None
             or self.control_std is None
         ):
@@ -247,8 +249,8 @@ class SwitchingLSTMBaseModel(base.DynamicIdentificationModel):
         with open(file_path[2], mode='w') as f:
             json.dump(
                 {
-                    'state_mean': self.state_mean.tolist(),
-                    'state_std': self.state_std.tolist(),
+                    'output_mean': self.output_mean.tolist(),
+                    'output_std': self.output_std.tolist(),
                     'control_mean': self.control_mean.tolist(),
                     'control_std': self.control_std.tolist(),
                 },
@@ -264,8 +266,8 @@ class SwitchingLSTMBaseModel(base.DynamicIdentificationModel):
         )
         with open(file_path[2], mode='r') as f:
             norm = json.load(f)
-        self.state_mean = np.array(norm['state_mean'], dtype=np.float64)
-        self.state_std = np.array(norm['state_std'], dtype=np.float64)
+        self.output_mean = np.array(norm['output_mean'], dtype=np.float64)
+        self.output_std = np.array(norm['output_std'], dtype=np.float64)
         self.control_mean = np.array(norm['control_mean'], dtype=np.float64)
         self.control_std = np.array(norm['control_std'], dtype=np.float64)
 